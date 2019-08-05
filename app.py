6e935024b02a45c600c6094561da20eedeb53fbe
Jay <jaybailey@outlook.com>
2019-08-05 17:53:21 +0100

    Adding Database connection information to app.py and added test case to app.py to test connection on heroku with enviroment variables

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'cimilestone3'
+app.config["MONGO_URI"] = os.environ.get("MONGODB_URI")
+
 
 
 @app.route('/')
 def hello():
     return 'Hello World ...again'
 
+@app.route('/test')
+def dbtest():
+    return app.config['MONGO_URI']
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            )
+            debug=True)
