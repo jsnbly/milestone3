601d49e3394e927a1a4d69729471f27604a881af
Jason <jaybailey@outlook.com>
2019-09-11 12:50:04 +0100

    installed WTForms, setup base2.html to allow testing of form data, added forms.py to hold form class information, added an import for forms.py to app.py and added a route to login template.

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from forms import LoginForm
 
 app = Flask(__name__)
 
-#MongoDB Atlas config linked to Enviroment Variables on Heroku
+#MongoDB Atlas config linked to Enviroment Variables in Heroku Settings
 app.config["MONGO_DBNAME"] = 'cimilestone3'
 app.config["MONGO_URI"] = os.environ.get("MONGODB_URI")
+#secret key config linked to Enviroment Variable in Heroku Settings
+app.config["SECRET_KEY"]= os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
@@ -16,10 +19,17 @@ mongo = PyMongo(app)
 def index():
     return render_template("base.html")
 
+#User Route code
+
 #Get User Route
 @app.route('/get_user')
 def get_user():
     return render_template("user.html", users=mongo.db.user.find())
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 #Get Recipe Route
 @app.route('/get_recipe')
