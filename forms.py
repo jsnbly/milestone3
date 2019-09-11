ad9ef73ec03c1dd55fb11b482774cab464f87b2d
Jason <jaybailey@outlook.com>
2019-09-11 14:26:06 +0100

    Added Equalto Validator to forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired,Length,Email
+from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
