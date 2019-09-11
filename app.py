11ba9898a19c0c91a6a43ae9778505cfb9b2255a
Jason <jaybailey@outlook.com>
2019-09-11 17:30:54 +0100

    removed old requirements.txt from workspace renamed base.html to landing.html, renamed base2.html to base, added bcrypt to app.py and updated registration route to included password hashing, added link to login to prompt user to register if they do not have an account

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import bcrypt
 from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -12,50 +13,62 @@ app.config["MONGO_URI"] = os.environ.get("MONGODB_URI")
 #secret key config linked to Enviroment Variable in Heroku Settings
 app.config["SECRET_KEY"]= os.environ.get("SECRET_KEY")
 
+#Initilize Pymongo
 mongo = PyMongo(app)
 
 #Main Route
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("base2.html")
+    return render_template("landing.html", title='Lets get Cookin')
 
 #User Routes
 
-#Get User Route
+#Get User Route ADMIN
 @app.route('/get_user')
 def get_user():
     return render_template("user.html", users=mongo.db.user.find())
 
+#Login user
 @app.route('/login')
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template("login.html", title="Sign In", form=form)
 
+#register user
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = AddUser(request.form)
     if form.validate_on_submit():
         user = mongo.db.user
         dose_user_exist = user.find_one({'username':request.form['username']})
+        cryptpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrpyt.gensalt())
         if dose_user_exist is None:
             user.insert_one({'username':request.form['username'],
-                                'password': request.form['password'],
+                                'password': request.form['cryptpass'],
                                 'email':request.form['email']})
+            session['username'] = request.form['username']
             return redirect(url_for('index'))
         flash('That username already exists, Please try again')
         return redirect(url_for('register'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template("register.html", title='Register', form=form)
 
-#Get Recipe Route
+#Recipe Routes
+
+#Get Recipe Route ADMIN
 @app.route('/get_recipe')
 def get_recipe():
     return render_template("recp_view.html", recipes=mongo.db.recipe.find())
 
+#Add Recipe Route
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("add_recipe.html", title='Add a New Recipe')
+
 #Get Shop Route
 @app.route('/get_shop')
 def get_shop():
-    return render_template("shop.html", title ='Shop')
+    return render_template("shop.html", title='Shop')
 
 #Remove Debug flag for deployment
 if __name__ == '__main__':
