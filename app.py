a3e6ddae507960d748f406af3a34c087335fd497
Jason <jaybailey@outlook.com>
2019-09-11 18:59:50 +0100

    added render_template to login route

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def login():
             return redirect(url_for('index'))
         return 'Invalid Username or Password'    
 
-   # return render_template("login.html", title="Sign In", form=form)
+return render_template("login.html", title="Sign In", form=form)
 
 #register user
 @app.route('/register', methods=['GET', 'POST'])
